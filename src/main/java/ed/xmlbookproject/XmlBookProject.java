0039a57bd851765edab6f7de202160ee375b5492
matinampoz <matinampoz@gmail.com>
2024-09-07 20:39:33 +0300

    init commit

---
+++ src/main/java/ed/xmlbookproject/XmlBookProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ed.xmlbookproject;
+
+/**
+ *
+ * @author matin
+ */
+public class XmlBookProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
